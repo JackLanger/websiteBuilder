c40fc91f772ed26fc8f044dd34fc5432bc12ca2e
JackLanger <jacek.l271186@gmail.com>
2020-12-23 18:58:26 +0100

    fixed movement bug for move function regarding new divs

--- setup.js
+++ setup.js
@@ -3,6 +3,8 @@ const handle = document.querySelector(".handle");
 const toolsNav = document.querySelector(".tools_nav");
 const body = document.querySelector(".outputBody");
 
+var id = -1; 
+
 //transform nodelist to array
 
 var toolsArr = Array.from(toolsLink);
@@ -64,7 +66,7 @@ function resize() {
       sidebar.style.width = pos.x + "px";
       handle.left = pos.x + "px";
       body.style.width =
-        window.innerWidth - sidebar.getBoundingClientRect().left + "px";
+        window.innerWidth - sidebar.getBoundingClientRect().width + "px";
       body.style.height = window.innerHeight + "px";
     } else {
       sidebar.style.width = "50px";
@@ -88,17 +90,19 @@ function createElement() {
   });
 
   function click() {
-    //create the element
+    /**
+     * create the element
+     * */
+    ++id;
+
     let div = document.createElement("DIV");
     let dragArea = document.createElement("DIV");
+
+    div.className = "newDiv";
+    div.id = id;
+    dragArea.className = "grab";
+
     div.appendChild(dragArea);
-    dragArea.style.width = "90%";
-    dragArea.style.height = "90%";
-    dragArea.style.position = "relative";
-    dragArea.style.border = "#ddd dashed thin";
-    dragArea.style.left = "5%";
-    dragArea.style.top = "5%";
-    dragArea.style.boxSizing = "border-box";
 
     let borders = [
       document.createElement("DIV"),
@@ -133,103 +137,51 @@ function createElement() {
     });
 
     body.appendChild(div);
-
-    div.style.width = "150px";
-    div.style.height = "150px";
-    div.style.border = "thin solid #ced6e0";
-    div.style.position = "relative";
-    dragArea.addEventListener("mousedown", mousedown);
-
-    //borders for resizing
-    let rect = div.getBoundingClientRect();
-
-    function mousedown() {
+/**
+ * move the new divs by drag and drop
+ */
+    dragArea.addEventListener("mousedown", (e) => {
       //subscribe to events
       window.addEventListener("mousemove", mousemove);
       window.addEventListener("mouseup", mouseup);
 
       dragArea.style.cursor = "move";
-      div.style.position = "absolute";
-    }
 
-    //adjust the size of the div
-    function adjust() {
-      let id = this.id;
-      window.addEventListener("mouseup", mouseup);
-      window.addEventListener("mousemove", resize);
-        //check object for bugs
-      var divObj = {
-        x: rect.left,
-        x2:rect.left + rect.width,
-        y: rect.top,
-        y2:rect.top+rect.height,
-        width:rect.width,
-        height:rect.height
+      var sideWidth = sidebar.getBoundingClientRect().width;
+
+      let left = div.getBoundingClientRect().left - sideWidth;
+      let top = div.getBoundingClientRect().top;
+
+      offset = {
+        x: left - e.clientX,
+        y: top - e.clientY,
       };
 
-      function resize(e) {
+      function mousemove(e) {
         e.preventDefault();
 
-        let rect = div.getBoundingClientRect();
-
-        let pos = {
+        var pos = {
           x: e.clientX || e.pageX,
           y: e.clientY || e.pageY,
         };
-        /**
-         * resize the div take the original position and size for resizing
-         * need to define which elem was pressed for directional purpose
-         * do not shrink on initial click.. keep size as is unless user reduces size
-         */
-        if (id == "e" || id == "w") {
-          //resize horizontally
-          if (id == "e") {
-            div.style.width = pos.x - rect.left + "px";
-          } else {
-            /**
-             * move the div to the right while reducing the size
-             * bug moves the initial position back to 300 
-             */
-            let newPos = pos.x - divObj.x;
-            let newWidth = divObj.width - newPos;
 
-            div.style.left = newPos + "px";
-            div.style.width = newWidth + "px";
-          }
-        } else {
-          //resize vertically
-          if (id == "n") {
-          } else {
-            div.height = pos.y - divObj.y + "px";
-          }
-        }
+        let x = pos.x + offset.x;
+        let y = pos.y + offset.y;
+
+        div.style.left = x + "px";
+        div.style.top = y-(div.getBoundingClientRect().height*div.id) + "px";
+
+        console.log(div,x,y);
       }
 
       // unsubscribe from the events
       function mouseup() {
-        window.removeEventListener("mousemove", resize);
+        //div.style.left = recalcPosition()+"px";
+        //div.style.position = "relative";
+        window.removeEventListener("mousemove", mousemove);
         window.removeEventListener("mouseup", mouseup);
+        dragArea.style.cursor = "auto";
       }
-    }
-    /**
-     * move the div on the screen and drop it where the mouse button is released
-     */
-    function mousemove(e) {
-      e.preventDefault();
-
-      let pos = {
-        x: e.clientX || e.pageX,
-        y: e.clientY || e.pageY,
-      };
-
-      div.style.left = pos.x + "px";
-      div.style.top = pos.y + "px";
-    }
-
-    function mouseup() {
-      div.style.cursor = "auto";
-      window.removeEventListener("mousemove", mousemove);
-      window.removeEventListener("mouseup", mouseup);
-    }
+    });
   }
 }
